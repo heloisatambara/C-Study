a68e6504c38f473437f14a6ed7dc46d262a93f58
Heloisa Tambara <heloisatambara@usp.br>
2022-12-19 17:56:39 -0300

    iniciando o text editor

---
+++ TextEditor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void main() {
+    // para possibilitar o uso de acentos
+    setlocale(LC_ALL, "");
+
+    // criar variaveis
+    char input, texto;
+    input = ' ';
+
+    // recebe os comandos e chama as funcoes correspondentes
+    while (input != '!') {
+        
+    }
+}
