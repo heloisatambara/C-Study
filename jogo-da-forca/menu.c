487479c0af7bbb9c2e1d75d5da51eee6a6d9b627
Heloisa Tambara <heloisatambara@usp.br>
2023-01-03 10:34:04 -0300

    random is working

--- jogo-da-forca/menu.c
+++ jogo-da-forca/menu.c
@@ -1,22 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
+
+void playSolo(){
+    int theme, index = rand() % 5;
+    
+
+    printf("\n\nChose theme:");
+    printf("\n1- Food");
+    printf("\n2- Names");
+    printf("\n3- Animals");
+    printf("\n4- Colors");
+    printf("\nType your choice: ");
+
+    scanf("%d", &theme);
+
+    switch(theme){
+        case 1:
+        { // here the brackets define a scope where the label "case 1" points to, so that the variable can be declared before anything alse
+            char foods[5][15] = {"Avocado", "Hamburger", "Salad", "Cheescake", "Peanut butter"};
+            printf("%s\n\n", foods[index]);
+            break;
+        }
+             
+        case 2:
+        {
+            char names[5][15] = {"Yasmin", "Katherine", "Annelise", "Alexander", "Hamilton"};
+            printf("%s", names[index]);
+            break;
+        }
+
+        case 3:
+        {
+            char animals[5][15] = {"Crocodile", "Wolf", "Dolphin", "Hipoppotamus", "Rhino"};
+            printf("%s", animals[index]);
+            break;
+        }
+
+        case 4:
+        {
+            char colors[5][15] = {"Yellow", "Green", "White", "Black", "Blue"};
+            printf("%s", colors[index]);
+            break;
+        }
+
+        default:
+            break;
+    }
+
+}
+
+void chosePlayers(){
+    int players;
+    printf("\n1- Play solo");
+    printf("\n2- Multiplayer");
+    printf("\nType your choice: ");
+
+    scanf("%d", &players);
+
+    switch(players){
+        case 1:
+            playSolo();
+            break;
+        case 2:
+            // multiplayer();
+            break;
+        default:
+            break;
+    }
+}
 
 void startMenu(){
-    int opcao = 0;
+    int option = 0;
 
-    while (opcao < 2) {
+    while (option < 3) {
+        
         printf("Welcome to Hangman Game!");
         printf("\n1- Play");
         printf("\n2- About");
         printf("\n3- Quit");
         printf("\nType your choice: ");
 
-        scanf("%d", opcao);
+        scanf("%d", &option);
 
-        switch(opcao){
+        switch(option){
             case 1:
+                chosePlayers();
                 break;
             case 2:
                 break;
@@ -26,12 +97,16 @@ void startMenu(){
                 break;
         }
 
-        //printf("\e[1;1H\e[2J");
+     //   printf("\e[1;1H\e[2J");
     }
 }
 
 
+
 void main(){
+
+    srand((unsigned) time(NULL));
+
     startMenu();
 
     
--- testes.c
+++ testes.c
@@ -4,11 +4,6 @@
 #include <stdbool.h>
 
 void main() {
-    char pilha[3][10] ={};
-    int i=0;
-    // for (i=0; i<=2; i++) {
-    //     strcpy(pilha[i],"aa");
-    // }
-    //bool teste = pilha[0][0] == "0";
-    printf("%d", pilha[0]);
+    char pa[] = "aaa", b[] = "bbb";
+    printf("%s", pa);
 }
