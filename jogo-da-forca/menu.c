3fd577ffaec4a82d97948d5e5f28470776731435
Heloisa Tambara <heloisatambara@usp.br>
2023-01-03 10:59:31 -0300

    mask working

--- jogo-da-forca/menu.c
+++ jogo-da-forca/menu.c
@@ -2,12 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+/*
+    TO DO:
+    - remove undesirable prints on choosing words parts;
+    - Add a configuration part to the menu: add words to the solo bank of words.
+*/
 
+/// @brief Creates the mask and shows the word chosen by the singlePlayer() function or typed in by the multiplayer user.
+void showWord(char *word) {
+    int i = 0, wordLength = strlen(word), maskLength = wordLength*2;
+    char mask[maskLength]; 
 
-void playSolo(){
+    for (i=0; i<maskLength; i+=2) {
+        mask[i] = '_';
+        mask[i+1] = ' ';
+    }
+
+    printf("%s", mask);
+
+}
+
+/// @brief For single player, shows options of themes that the word will belong to, and the function randomly chooses one from the bank of the chosen theme.
+void singlePlayer() {
     int theme, index = rand() % 5;
-    
+    char *word;
 
+    // selection menu
     printf("\n\nChose theme:");
     printf("\n1- Food");
     printf("\n2- Names");
@@ -17,11 +37,14 @@ void playSolo(){
 
     scanf("%d", &theme);
 
-    switch(theme){
+    // action corresponding to option chosen by use
+    switch(theme) {
         case 1:
-        { // here the brackets define a scope where the label "case 1" points to, so that the variable can be declared before anything alse
+        { // here the brackets define a scope where the label "case 1" points to, so that the variable can be declared before anything else
+            // because variables can't be defined right after labels, but in scopes there's no problem.
             char foods[5][15] = {"Avocado", "Hamburger", "Salad", "Cheescake", "Peanut butter"};
             printf("%s\n\n", foods[index]);
+            word = foods[index];
             break;
         }
              
@@ -50,19 +73,24 @@ void playSolo(){
             break;
     }
 
+    showWord(word);
 }
 
-void chosePlayers(){
+/// @brief If the user chose to play, here they choose if it's a single or multi player.
+void chosePlayers() {
     int players;
-    printf("\n1- Play solo");
+
+    // selection menu
+    printf("\n1- Single player");
     printf("\n2- Multiplayer");
     printf("\nType your choice: ");
 
     scanf("%d", &players);
 
+    // action corresponding to option chosen by use
     switch(players){
         case 1:
-            playSolo();
+            singlePlayer();
             break;
         case 2:
             // multiplayer();
@@ -72,11 +100,13 @@ void chosePlayers(){
     }
 }
 
-void startMenu(){
+/// @brief Shows first options that the user can choose: play, see info about the game or quit. This loops until the user quits.
+void startMenu() {
     int option = 0;
 
     while (option < 3) {
         
+        // selection menu
         printf("Welcome to Hangman Game!");
         printf("\n1- Play");
         printf("\n2- About");
@@ -85,6 +115,7 @@ void startMenu(){
 
         scanf("%d", &option);
 
+        // action corresponding to option chosen by use
         switch(option){
             case 1:
                 chosePlayers();
@@ -102,14 +133,9 @@ void startMenu(){
 }
 
 
+/// @brief Main function.
+void main() {
+    srand((unsigned) time(NULL)); // if user choses play solo, a random word will be chosen
 
-void main(){
-
-    srand((unsigned) time(NULL));
-
-    startMenu();
-
-    
-    
-
+    startMenu(); // calls for the main menu
 }
